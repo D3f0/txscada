4eab0b84a001abb1f3893dcc3fd29fb82db98079
nahuel <nahuel@asgard-jaunty>
2009-08-06 21:43:23 -0300

    Agregamos CherryPy como host WSGI puramente Python

--- src/restscada/core/protocols/picnet.py
+++ src/restscada/core/protocols/picnet.py
@@ -6,3 +6,9 @@ Created on 06/08/2009
 Implementacion del protcolo PICNET para RESTSCADA.
 '''
 
+from twisted.internet import reactor, protocol
+from bitstring import BitString
+
+
+class PicnetProtocol(protocol.Protocol):
+    pass
