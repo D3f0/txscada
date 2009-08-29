2717349a650ce61c7343a13160289f36dde7a381
nahuel <nahuel@asgard-jaunty>
2009-08-29 18:57:17 -0300

    Redireccion

--- src/restscada/main.py
+++ src/restscada/main.py
@@ -3,7 +3,15 @@ Created on 06/08/2009
 
 @author: defo
 '''
+import sys
+try:
+    import restscada
+except ImportError:
+    sys.path.append('..')
+
 from restscada.rest.site import site
+from twisted.internet import reactor
+
 if __name__ == "__main__":
     # TODO: Paramtros de linea de comandos
     # TODO: Cargar configuracion basada en objeto config.Config
--- src/restscada/rest/site.py
+++ src/restscada/rest/site.py
@@ -4,14 +4,54 @@ Created on 29/08/2009
 @author: defo, lau
 '''
 
-from twisted.web import server
-from twisted.web import resource
+from twisted.web import server, resource, static
 
-class BaseResource( resource.Resource ):
-    pass
 
+class UserAgentDelegate(object):
+    def render(self):
+        raise NotImplementedError("You must sublcass this method")
+
+#class HTMLDelegate():
+
+class RESTfulResource( resource.Resource ):
+    
+    
+    def render(self, request):
+        
+        response_delegate = self.responseDelegateFactory(request.method)
+        
+        try:
+            return response_delegate.render()
+        except Exception, e:
+            return "Error"
+    
+#    def __getattr__(self, name):
+#        index = name.index('render_')
+#        if index:
+#            method_name = name[index:]
+#            xxxx.yyyy
+
+class Redirector(resource.Resource):
+    def __init__(self, to, **kwargs):
+        self.to = to
+        
+    def render(self, response):
+        response.setResponseCode(301)
+        response.setHeader('Location', self.to)
+        response.finish()
+
+class BaseResource(resource.Resource):
+    
+    def __init__(self, *largs, **kwargs):
+        resource.Resource.__init__(self, *largs, **kwargs)
+        static_res = static.File('static')
+        self.putChild('static', static_res )
+        self.putChild('', Redirector('/static/'))
+    
 base_resource = BaseResource()
+site = server.Site(base_resource)
 
-site = server.Site( base_resource )
+
+
 
 
