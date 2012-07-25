0d870091b9260bbb2625ea3fdc112583d9bfea23
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-07-25 09:29:44 -0300

    Fabric/Rails inspired way to invoke commands instead of server reload -> s:r

--- src/pysmve/commands.py
+++ src/pysmve/commands.py
@@ -21,14 +21,59 @@ def command(obj):
     COMMANDS[obj.func_name] = obj
     return wrapped
     
+def open_local_browser(port):
+    print "Lanzando browser"
+    import webbrowser
+    webbrowser.open('http://localhost:%d/' % port)
+
+
+@command
+def server(options, restart=False):
+    from app import app
+    print "Running server"
+    if options.reload:
+        # Con flask
+        print "Trabajando con flask stand-alone"
+        import gevent.wsgi
+        from gevent import monkey
+        import werkzeug.serving
+        werkzeug.serving    
+        # http://flask.pocoo.org/snippets/34/ (2nd comment)
+        monkey.patch_all()
+
+        @werkzeug.serving.run_with_reloader
+        def runServer():
+            app.debug = True
+            ws = gevent.wsgi.WSGIServer(('', options.port), app)
+            ws.serve_forever()
+        
+        app.run()
+
+    else:
+        # Con Twisted
+        from resources import site
+        from twisted.internet import reactor
+        
+        from models import COMaster
+        for x in COMaster.select():
+            print x
+        
+        reactor.listenTCP(options.port, site)
+        reactor.callLater(1, partial(open_local_browser, port=options.port))    
+        print "Iniciando servidor en http://0.0.0.0:%s" % options.port
+        reactor.run()
     
 @command
-def server(options):
-    print "server"
+def server_plus(options):
+    print "Server plus"
     
 @command
 def dbshell(options):
-    print "dbshell"
+    pass
+    
+@command
+def syncdb(options, recreate=False):
+    pass
     
 
 
--- src/pysmve/main.py
+++ src/pysmve/main.py
@@ -3,57 +3,99 @@
 
 import sys
 from functools import partial
+import inspect
+from copy import copy
 
-def check():
-    import os
-    assert 'VIRTUAL_ENV' in os.environ, "Falta el virtualenv"
 
-def open_local_browser(port):
-    print "Lanzando browser"
-    import webbrowser
-    webbrowser.open('http://localhost:%d/' % port)
+def coincidence(s1, s2):
+    counter = 0
+    for c1, c2 in zip(s1, s2):
+        if not c1 == c2: break
+        counter += 1
+    return counter
+
+def choose(ref, names):    
+    coincidences = map(lambda s: coincidence(ref, s), names)
+    return names[coincidences.index(max(coincidences))]
+    
+def choose_pop(starting, listofargs):
+    '''
+    Removes most similar word from listofargs with startswith(starting)
+    criteria.
+    Ex: choose_pop('ab', ['abc', 'def']) => returns abc, listofargs ['def']
+    '''
+    coincidences = map(lambda s: coincidence(starting, s), listofargs)
+    mostlikely = max(coincidences)
+    if not mostlikely:
+        raise ValueError("Argument error with %s" % starting)
+    index = coincidences.index(mostlikely) # Most likely word maximises
+    value = listofargs.pop(index)
+    return value
+    
+    
+def make_kwargs(func, strofargs):
+    ''' Geneate kwargs dict based on function argspec'''
+    if not strofargs: return {}
+    
+    argspec = inspect.getargspec(func)
+    args = copy(argspec.args[1:]) # Options is always the first argument
+    kwargs = {}
+    parts = strofargs.split(':')
+    for arg in parts:
+        if not len(arg): continue
+        if '=' in arg:
+            k, v = arg.split('=')
+        else:
+            k, v = (arg, arg)
+    
+    kwargs.update({choose_pop(k, args): v})
+    return kwargs
+
+def runcommand(cmdline, options, command_dict):
+    """Split commands in fabric style,
+    returns (cmdname, cmdkwargs)"""
+    from difflib import get_close_matches
+    if ':' in cmdline:
+        cmdname, args = cmdline.split(':', 1)
+    else:
+        cmdname, args = cmdline, ''
+    print cmdname, "-", args
+    command_name = get_close_matches(cmdname, command_dict.keys())
+    n = len(command_name)
+    if n == 0:
+        raise KeyError("No command named %s" % cmdname)
+    else:
+        name = command_name[0]
+        if n > 1:
+            print "Choosing %s from %s" % (name, ', '.join(command_name))
         
+        command_func = command_dict[command_name[0]]
+    kwargs = make_kwargs(command_func, args)
+    print kwargs
+    return command_func(options, **kwargs)
+    
+    
+        
+    
+    
 
 def main(argv = sys.argv):
     # Aplicación
     
-    from commands import command
-    
-    from app import app
+    from commands import COMMANDS
     from options import parser
-
+    
     options = parser.parse_args()
-    if options.reload:
-        # Con flask
-        print "Trabajando con flask stand-alone"
-        import gevent.wsgi
-        from gevent import monkey
-        import werkzeug.serving
-        werkzeug.serving    
-        # http://flask.pocoo.org/snippets/34/ (2nd comment)
-        monkey.patch_all()
+    try:
+        return runcommand(options.command[0], options, command_dict=COMMANDS)
+    except Exception as e:
+        print e
+        from traceback import format_exc
+        print format_exc()
+        return -1
+    
+    
 
-        @werkzeug.serving.run_with_reloader
-        def runServer():
-            app.debug = True
-            ws = gevent.wsgi.WSGIServer(('', options.port), app)
-            ws.serve_forever()
-        
-        app.run()
-
-    else:
-        # Con Twisted
-        from resources import site
-        from twisted.internet import reactor
-        
-        from models import COMaster
-        for x in COMaster.select():
-            print x
-        
-        reactor.listenTCP(options.port, site)
-        reactor.callLater(1, partial(open_local_browser, port=options.port))    
-        print "Iniciando servidor en http://0.0.0.0:%s" % options.port
-        reactor.run()
 
 if __name__ == "__main__":
     sys.exit(main())
