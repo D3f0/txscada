2049d83f93a5455c624ad4f415db8ac3856c1a0d
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-08-03 13:10:54 -0300

    Probando con las tramas que envió ricardo en el mail del 3/Ago

--- src/pysmve/protocols/constructs.py
+++ src/pysmve/protocols/constructs.py
@@ -64,7 +64,7 @@ Event = Struct("event",
 #===============================================================================
 # Payload del comando 10 - Encuesta de energías al COMaster
 #===============================================================================
-Payload_10 = Struct("Payload_10",
+Payload_10 = Struct("payload_10",
     ULInt8('canvarsys'),
     Array(lambda ctx: ctx.canvarsys / 2, ULInt16('varsys')),
     ULInt8('candis'),
@@ -85,7 +85,7 @@ MaraFrame = Struct('Mara',
             ULInt8('sequence'),
             ULInt8('command'),
             Optional(Payload_10),
-            ULInt8('bcc')
+            ULInt16('bcc')
 )
 
 def ints2buffer(hexstr):
@@ -100,6 +100,7 @@ def hexstr2buffer(a_str):
     "FE  01" => '\xFE\x01'    
     '''
     import re
+    a_str = a_str.strip().replace('\n', ' ')
     a_list = [ chr(int(bytestr, 16)) for bytestr in  re.split('[:\s]', a_str) if len(bytestr) ]
     return ''.join(a_list)
 
@@ -134,7 +135,7 @@ def dtime2dict(dtime = None):
     return d    
 
 
-def build_mara_frame(obj, subcon=MaraFrame):
+def build_frame(obj, subcon=MaraFrame):
     '''Generates a mara frame, with checksum and qty'''
     stream = subcon.build(obj)
     data= "".join([
@@ -145,14 +146,37 @@ def build_mara_frame(obj, subcon=MaraFrame):
     cs = make_cs_bigendian(data)
     cs_str = Array(2, Byte('cs')).build(cs)
     return "".join((data, cs_str))
-    
 
-if __name__ == '__main__':
-    #===========================================================================
-    # Debug with ipython --pdb -c "%run constructs.py"
-    #===========================================================================
-    
-    import sys
+def parse_frame(buff, as_hex_string=False):
+    if as_hex_string:
+        buff = hexstr2buffer(buff)
+    data = MaraFrame.parse(buff)
+    return data
+
+def format_frame(buff, as_hex_string=False):
+    d = parse_frame(buff, as_hex_string)
+    print "SOF:", d.sof
+    print "SEQ:", d.length
+    print "DST:", d.dest
+    print "SRC:", d.source
+    print "SEQ:", d.sequence
+    print "CMD:", d.command
+    # Payload
+    if d.payload_10:
+        p = d.payload_10
+        print "%12s" % "CANVARSYS:", p.canvarsys, "%d valores de word de 16" % (p.canvarsys/2)
+        print "%12s" % "VARSYS:",    p.varsys 
+        print "%12s" % "CANDIS:",    p.candis, "%d valores de word de 16" % (p.candis/2) 
+        print "%12s" % "DIS:", p.dis
+        print "%12s" % "CANAIS:", p.candis, "%d valores de word de 16" % (p.canais/2)
+        print "%12s" % "DIS:", p.ais
+        # Eventos
+    print "BCC:", d.bcc
+        
+
+def test():
+    '''Testing de tramas y subtramas'''
+
     int2str = lambda l: ''.join(map(chr, l))
     #int2strgen = lambda *l: (chr(i) for i in l)
     result = MaraFrame.parse(any2buffer('FE    08    01    40    80    10    80    A7'))
@@ -185,11 +209,60 @@ if __name__ == '__main__':
     pkg = Payload_10.build(payload_10_data)
     print upperhexstr(pkg)
     frame_data = Container(sof=0xFE, length=0, source=1, dest=2, sequence=0x80, command=0x10, 
-                                    Payload_10=payload_10_data,
+                                    payload_10=payload_10_data,
                                     bcc=0)
     pkg = MaraFrame.build(frame_data)
     
     print "Trama Mara c/QTY=0 y sin CS: ",  upperhexstr(pkg)
-    print "Trama completa:", upperhexstr(build_mara_frame(frame_data))
+    print "Trama completa:", upperhexstr(build_frame(frame_data))
+
+if __name__ == '__main__':
+    #===========================================================================
+    # Debug with ipython --pdb -c "%run constructs.py"
+    #===========================================================================
+    
+    import sys
+    
+    # test()
+    
+    print "-"*80
+    print "Trama 1"
+    print "-"*80
+    trama_1 = """
+    FE 44 40 01 4A 10 19 00 00 90 1D 01 00 00 00 00 00 80 80 00 00 80 80 00 00 80 80 00
+    00 80 80 0F 00 00 43 00 00 00 00 04 00 04 00 04 00 04 13 48 05 51 00 51 00 51 00 51
+    00 51 00 51 00 51 00 51 00 01 E1 29
+    """
+    # Primera trama
+    format_frame(trama_1, as_hex_string=True)
+    
+    
+    print "-"*80
+    print "Trama 2"
+    print "-"*80
+    trama_2 = """
+    FE 44 40 01 4C 10 19 00 00 85 1D 01 00 00 00 00 00 00 01 00 00 00 01 00 00 00 01 00 00 00 01 0F 
+    00 00 43 00 00 00 00 04 00 04 00 04 00 04 13 4C 05 51 00 51 00 51 00 51 00 51 00 51 00 51 00 51 
+    00 01 DD 30
+    """
+    # Segunda trama
+    format_frame(trama_2, as_hex_string=True)
+
+    print "-"*80
+    print "Trama 3"
+    print "-"*80
+    trama_3 = """
+    FE F8 40 01 4D 10 19 00 00 8D 1D 01 00 00 00 00 00 00 01 00 00 00 01 00 00 00 01 00 00 00 01 0F
+    00 00 43 00 00 00 00 04 00 04 00 04 00 04 13 81 09 51 00 51 00 51 00 51 00 51 00 51 00 51 00 51
+    00 B5 01 93 0C 01 01 01 08 22 00 14 01 E3 0C 01 01 01 08 22 00 14 01 05 0C 01 01 01 08 22 00 14 
+    01 92 0C 01 01 01 08 22 00 18 01 E2 0C 01 01 01 08 22 00 18 01 04 0C 01 01 01 08 22 00 18 01 F1 
+    0C 01 01 01 08 22 00 1C 01 93 0C 01 01 01 08 22 00 1C 01 F3 0C 01 01 01 08 22 00 1C 01 05 0C 01 
+    01 01 08 22 00 1C 01 F2 0C 01 01 01 08 22 00 20 01 04 0C 01 01 01 08 22 00 20 01 F0 0C 01 01 01 
+    08 22 00 24 01 92 0C 01 01 01 08 22 00 24 01 15 0C 01 01 01 08 22 00 60 01 14 0C 01 01 01 08 22 
+    00 7C 01 F3 0C 01 01 01 08 23 00 00 01 15 0C 01 01 01 08 23 00 00 3C 91
+    """
+    # Tercer trama
+    format_frame(trama_3, as_hex_string=True)
+    
     
     
