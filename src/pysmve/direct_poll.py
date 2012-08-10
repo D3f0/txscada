1cfa86c3280476e8e9405c13178bfbdfb0ee86f0
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-08-10 09:04:42 -0300

    Agregando archivos que no estaban en el indice

---
+++ src/pysmve/direct_poll.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+from socket import socket
+from protocols.constructs import MaraFrame
+from construct.lib.container import Container
+from time import sleep
+
+
+
+
+def main():
+    while True:
+        s = socket()
+        s.connect(('192.168.1.97', 9761))
+        paquete = MaraFrame.build(Container(
+                                              source=64,
+                                              dest=1,
+                                              sequence=33,
+                                              command=0x10,
+                                              payload_10 = None
+                                              ))
+        print paquete, type(paquete)
+        s.send(str(paquete))
+        data = s.recv(1024)
+        print MaraFrame.parse(data)
+        sleep(1)
+    
+main()
