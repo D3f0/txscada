f37865bce34fa4c8ef5756f2ba603ca21d0b9f2f
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-11-07 01:41:45 -0300

    Helper function

--- src/pysmve/protocols/utils/__init__.py
+++ src/pysmve/protocols/utils/__init__.py
@@ -5,7 +5,11 @@ def ints_from_str(hexstr):
     parts = re.split(r'[\s:\t]', hexstr) 
     for part in filter(len, parts):
         yield int(part, 16)
-        
+
+def format_buffer(buff):
+    '''Buffer to human readable hex representation'''
+    s = " ".join(("%.2x" % one_byte for one_byte in map(ord, buff)))
+    return s.upper()
 
 if __name__ == '__main__':
     print list(ints_from_str('FE    08    01    40    80    10    80    A7'))
