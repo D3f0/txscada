1735c1f51f5e0186cdbc4c1cee5b566a2c8add26
Marcos Paredes <marcosparedes.tw@gmail.com>
2012-12-17 12:04:35 -0300

    Migraciones

--- src/pysmve/nguru/mara/models.py
+++ src/pysmve/nguru/mara/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    name = models.CharField(max_length=100)
+    enabled = models.BooleanField(default=True)
+    
+    
+class COMaster(models.Model):
+    profile = models.ForeignKey(Profile)
+    ip_address = models.IPAddressField()
+    
+    
+    
--- src/pysmve/nguru/nguru/settings.py
+++ src/pysmve/nguru/nguru/settings.py
@@ -11,8 +11,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'txscada',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -119,6 +119,8 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'south',
+    'mara',
 )
 
 # A sample logging configuration. The only tangible logging
