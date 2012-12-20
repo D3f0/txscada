a76d44f20d7eb35e385ad05e187313b23e470735
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-12-20 15:38:31 -0300

    Instalación de SocketIO y Traducción al castellano

--- src/pysmve/nguru/nguru/settings.py
+++ src/pysmve/nguru/nguru/settings.py
@@ -30,11 +30,11 @@ MANAGERS = ADMINS
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
 # In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-ar'
 
 SITE_ID = 1
 
@@ -121,11 +121,13 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #'admin_bootstrap',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
     'django_extensions',
+    'django_socketio',
 
     'mara',
 )
--- src/pysmve/nguru/nguru/urls.py
+++ src/pysmve/nguru/nguru/urls.py
@@ -9,10 +9,13 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'nguru.views.home', name='home'),
     # url(r'^nguru/', include('nguru.foo.urls')),
-
+    url("", include('django_socketio.urls')),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin', site.urls),
-
+    url('^$', 'django.views.generic.simple.direct_to_template',
+        {
+         'template': 'index.html'
+         })
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
