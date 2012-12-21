b6da738295f94ad7f5550643fb89d21a82a974d0
Nahuel Defosse <nahuel.defosse@gmail.com>
2012-12-21 03:04:32 -0300

    Pequeños cambios en los protocolos para que dialoguen

--- src/pysmve/protocols/mara/client.py
+++ src/pysmve/protocols/mara/client.py
@@ -13,6 +13,7 @@ from ..utils.bitfield import bitfield
 from ..constructs import upperhexstr
 import models
 from protocols.utils import format_buffer
+from protocols.utils.bitfield import iterbits
 
 logger = getLogger(__name__)
 
@@ -22,11 +23,11 @@ class MaraClientProtocol(protocol.Protocol):
     Communitcation with one COMaster.
     A COMaster actas as a gateway with RS485 operational bay networks.
     '''
-    
+
     CLIENT_STATES = set(['IDLE', 'RESPONSE_WAIT', ])
-    
+
     save_events = True
-    
+
     def __init__(self, factory):
         self.factory = factory
         self.state = 'IDLE'
@@ -34,28 +35,28 @@ class MaraClientProtocol(protocol.Protocol):
         self.timeouts = 0
         # Data to be sent to COMaster
         self.output = Container(
-                                source = self.factory.comaster.source,
-                                dest   = self.factory.comaster.dest,
-                                sequence = self.factory.comaster.sequence,
-                                command = 0x10,
-                                payload_10 = None, # No payload,
+                                source=self.factory.comaster.rs485_source,
+                                dest=self.factory.comaster.rs485_destination,
+                                sequence=self.factory.comaster.sequence,
+                                command=0x10,
+                                payload_10=None, # No payload,
                                 #peh=None,
                                 )
         # Data to be received from COMaster
         self.input = Container()
         self.poll_timer = LoopingCall(self.pollTimerEvent)
-        self.poll_timer.start(interval = self.factory.comaster.timeout, now=False)
-        
+        self.poll_timer.start(interval=self.factory.comaster.poll_interval, now=False)
+
         self.peh_timer = LoopingCall(self.pehTimerEvent)
-        self.peh_timer.start(interval=self.factory.comaster.timeout*0.5, now=False)
-        
+        self.peh_timer.start(interval=self.factory.comaster.poll_interval * 0.5, now=False)
+
         reactor.callLater(0.1, self.pehTimerEvent)
-        
-        
+
+
     def connectionMade(self):
         logger.debug("Conection made to %s:%s" % self.transport.addr)
         reactor.callLater(0.01, self.sendCommand) #@UndefinedVariable
-    
+
     def getPEHContainer(self):
         return Container(
                          source=self.output.source,
@@ -64,33 +65,33 @@ class MaraClientProtocol(protocol.Protocol):
                          command=0x12,
                          peh=datetime.now()
                          )
-    
+
     def pehTimerEvent(self):
         '''Evento que inidica que se debe enviar la puesta en hora'''
-        
+
         if self.state == 'IDLE':
             buffer = self.construct.build(self.getPEHContainer())
             self.transport.write(buffer)
             print "PEH >>", format_buffer(buffer)
-    
+
     def pollTimerEvent(self):
         '''Event'''
         if self.pending == 0:
-            print "Sending command to:",  self.factory.comaster.address, self.factory.comaster.sequence
+            print "Sending command to:", self.factory.comaster.ip_address, self.factory.comaster.sequence
         else:
             print "Sending retry %s %d" % (self.factory.comaster, self.pending)
-            
+
         self.sendCommand()
-    
+
     def sendCommand(self):
         # Send command
-        
+
         frame = MaraFrame.build(self.output)
         self.transport.write(frame)
         self.state = 'RESPONSE_WAIT'
         self.pending += 1
-        
-        
+
+
     def dataReceived(self, data):
         if self.state == 'IDLE':
             logger.warning("Discarding data in IDLE state %d bytes" % len(data))
@@ -114,95 +115,86 @@ class MaraClientProtocol(protocol.Protocol):
             self.pending = 0
             #from IPython import embed; embed()
             deferToThread(self.saveInDatabase)
-            
+
             #print self.input
             print self.transport.addr, format_buffer(data)
             MaraFrame.pretty_print(self.input, show_header=False, show_bcc=False)
             self.state = 'IDLE'
-    
+
     def saveInDatabase(self):
         print "Acutalizando DB"
         #print self.input
         from models import DI, AI, VarSys, Energy, Event
         payload = self.input.payload_10
         comaster = self.factory.comaster
-        
+
         # Iterar de a bit
-        
-        def iterbits(ints, length=16):
-            for val in ints:
-                bf = bitfield(val)
-                for i in range(length):
-                    retval =  bf[i]
-                    #print retval
-                    yield retval
-        
-        
+
         def iterdis():
             # Iterar ieds
             for ied in self.factory.comaster.ied_set.order_by('offset'):
                 # Ordenar por puerto y por bit
-                for di in DI.filter(ied = ied).order_by(('port', 'asc'), ('bit', 'asc')):
+                for di in DI.filter(ied=ied).order_by(('port', 'asc'), ('bit', 'asc')):
                     yield di
         def iterais():
             # Iterar ieds
             for ied in self.factory.comaster.ied_set.order_by('offset'):
                 # Itera por ais
-                for ai in AI.filter(ied = ied).order_by('offset'):
+                for ai in AI.filter(ied=ied).order_by('offset'):
                     yield ai
-        
+
         def itervarsys():
             # Iterar ieds
             for ied in self.factory.comaster.ied_set.order_by('offset'):
-                for varsys in VarSys.filter(ied = ied).order_by('offset'):
+                for varsys in VarSys.filter(ied=ied).order_by('offset'):
                     yield varsys
-        
+
         #=======================================================================
         # Guardando...
         #=======================================================================
         for varsys, value in zip(itervarsys(), payload.varsys):
             varsys.value = value
             varsys.save()
-                            
+
         for di, value in zip(iterdis(), iterbits(payload.dis)):
             di.value = value
             di.save()
-        
+
         for ai, value in zip(iterais(), payload.ais):
             ai.value = value
             ai.save()
-            
+
         if self.save_events:
             for ev in payload.event:
                 if ev.evtype == "DIGITAL":
-                    ied = self.factory.comaster.ied_set.get(addr_485_IED = ev.addr485)
+                    ied = self.factory.comaster.ied_set.get(addr_485_IED=ev.addr485)
                     di = DI.get(ied=ied, port=ev.port, bit=ev.bit)
                     #di = comaster.dis.get(port = ev.port, bit = ev.bit)
-                    timestamp = datetime(ev.year+2000, ev.month, ev.day, ev.hour, ev.minute, ev.second,int(ev.subsec*100000))
-                    Event(di = di, 
-                          timestamp = timestamp, 
-                          subsec=ev.subsec, 
-                          q=0, 
-                          value = ev.status).save()
-                    
-                    
+                    timestamp = datetime(ev.year + 2000, ev.month, ev.day, ev.hour, ev.minute, ev.second, int(ev.subsec * 100000))
+                    Event(di=di,
+                          timestamp=timestamp,
+                          subsec=ev.subsec,
+                          q=0,
+                          value=ev.status).save()
+
+
                 elif ev.evtype == "ENERGY":
-                    timestamp = datetime(ev.year+2000, ev.month, ev.day, ev.hour, ev.minute, ev.second)
-                    ied = self.factory.comaster.ied_set.get(addr_485_IED = ev.addr485)
-                    Energy(ied=ied, 
-                           q=ev.value.q, 
-                           timestamp=timestamp, 
-                           address=ev.addr485, 
-                           channel=ev.channel, 
+                    timestamp = datetime(ev.year + 2000, ev.month, ev.day, ev.hour, ev.minute, ev.second)
+                    ied = self.factory.comaster.ied_set.get(addr_485_IED=ev.addr485)
+                    Energy(ied=ied,
+                           q=ev.value.q,
+                           timestamp=timestamp,
+                           address=ev.addr485,
+                           channel=ev.channel,
                            value=ev.value.val,).save()
-                    
+
             print ("Guardados %d eventos" % len(payload.event))
-        
+
     __state = 'IDLE'
     @property
     def state(self):
         return self.__state
-    
+
     @state.setter
     def state(self, value):
         assert value in self.CLIENT_STATES, "Invalid state %s" % value
@@ -210,68 +202,86 @@ class MaraClientProtocol(protocol.Protocol):
         # Fix this horrible code!!!
         if value == 'IDLE':
             if self.queued:
-                
+
                 self.transport.write(self.construct.build(self.queued))
                 self.queued = None
-    
+
     __factory = None
     @property
     def factory(self):
         return self.__factory
-    
+
     @factory.setter
     def factory(self, value):
         assert isinstance(value, ClientFactory)
         self.__factory = value
 
-    __construct = None    
+    __construct = None
     @property
     def construct(self):
         return self.__construct
-    
+
     @construct.setter
     def construct(self, value):
         assert issubclass(value, Struct), "Se esperaba un Struct"
         self.__construct = value
-    
-class MaraClientDBUpdater(MaraClientProtocol): 
+
+class MaraClientDBUpdater(MaraClientProtocol):
     '''
     This protocols saves data from scans into the
-    database using Peewee ORM. This may change 
+    database using Peewee ORM. This may change
     in the future.
     '''
-    
+    def saveInDatabase(self):
+        payload = self.input.payload_10
+        di_count, ai_count = 0, 0
+
+        for value, di in zip(iterbits(payload.dis),
+                                         self.factory.comaster.dis):
+            di.value = value
+            di.save()
+            di_count += 1
+
+        for value, ai in zip(payload.ais,
+                            self.factory.comaster.ais):
+            ai.value = value
+            ai.save()
+            ai_count += 1
+
+        print "Update DB: DI: %d AI: %d" % (di_count, ai_count)
+
+
 class MaraClientProtocolFactory(protocol.ClientFactory):
-    '''
-    Cliente de consultas a las placas de desarrollo
-    o a un emulador'''
-    
+    '''Creates Protocol instances to interact with mara servers'''
+
     protocol = MaraClientProtocol
-    
-    def __init__(self, comaster):
+
+    def __init__(self, comaster, reconnect=True):
         self.comaster = comaster
-    
+        self.reconnect = reconnect
+
     def buildProtocol(self, *largs):
-        p = MaraClientProtocol(factory = self)
+        p = self.protocol(factory=self)
         # TODO: Make this dynamic
         p.construct = MaraFrame
         return p
-        
+
     def clientConnectionFailed(self, connector, reason):
         #logger.warn("Connection failed: %s" % reason)
         print "Connection failed: %s" % reason
         print "Restarting"
-        connector.connect()
-        #reactor.stop()
-        
+        if self.reconnect:
+            connector.connect()
+        else:
+            reactor.stop()
+
     def clientConnectionLost(self, connector, reason):
         from twisted.internet import error
         logger.warn("Connection lost: %s" % reason)
         if reason.type == error.ConnectionLost:
             return
-        print "Connection lost: %s. Restarting..." % reason
-        connector.connect()
-        #reactor.stop()
-    
-    def startedConnecting(self, connector):
-        logger.debug("Started connecting")
+        if self.reconnect:
+            print "Connection lost: %s. Restarting..." % reason
+            connector.connect()
+        else:
+            reactor.stop()
--- src/pysmve/protocols/mara/server.py
+++ src/pysmve/protocols/mara/server.py
@@ -13,6 +13,8 @@ from datetime import datetime
 from ..utils.bitfield import bitfield
 from ..constructs import upperhexstr
 import models
+from copy import copy
+import random
 
 logger = getLogger(__name__)
 
@@ -23,21 +25,21 @@ logger = getLogger(__name__)
 class MaraServer(protocol.Protocol):
     '''
     Works as COMaster development board
-    It replies commands 0x10 based on the definition 
+    It replies commands 0x10 based on the definition
     in the comaster instance (a DB table).
     '''
     comaster = None
     def __init__(self):
         """Crea un protocolo que emula a un COMaster"""
-        self.input = None
-        self.output = None
-        
-        
+        self.input = Container()
+        self.output = Container()
+
+
     def connectionMade(self,):
         """docstring for connectionMade"""
         #from ipdb import set_trace; set_trace()
         logger.debug("Conection made to %s:%s" % self.transport.client)
-    
+
     def dataReceived(self, data):
         """Recepción de datos"""
         try:
@@ -46,36 +48,42 @@ class MaraServer(protocol.Protocol):
             # If the server has no data, it does not matter
             logger.warn("Error de pareso: %s" % upperhexstr(data))
         self.maraPackageReceived()
-    
+
     def maraPackageReceived(self):
-        pass
         """Note: Input holds input package parse results"""
         if self.input.command == 0x10:
             # Response for command 0x10
-            self.transport.write(self.makeResponse10)
-    
+            print "Responding Mara Frame from: %s" % self.transport
+            self.transport.write(self.makeResponse10())
+        else:
+            print "Not responding to package %x" % self.input.command
+
     def makeResponse10(self):
-        pass
-    
+        self.output = copy(self.input)
+        self.output.source, self.output.dest = self.output.dest, self.output.source
+
+        ais = [ random.randrange(0, 254) for _ in xrange(9) ]
+
+        self.output.payload_10 = Container(
+            canvarsys=5,
+            varsys=[0x1234, 0xfeda],
+            candis=4,
+            dis=[0x45AA, 0xFF],
+            canais=2 * len(ais) + 1, # Two byte vars
+            ais=ais,
+            canevs=0,
+            event=[]
+        )
+
+        return MaraFrame.build(self.output)
+
+
+
+
     def connectionLost(self, reason):
         print "Conexion con %s:%s terminada" % self.transport.client
 
-class MaraServerSimulator(MaraServer):
-    pass
 
 class MaraServerFactory(protocol.Factory):
     protocol = MaraServer
-    def __init__(self, comaster, *largs, **kwargs):
-        self.comaster = comaster
-        logger.debug("Conexion con %s" % comaster)
-        print comaster
-        
-    #def makeConnection(self, transport):
-    #    print "Make connection"
-    #    return protocol.Protocol.makeConnection(self, transport)
-    
 
-    
-
-
-        
