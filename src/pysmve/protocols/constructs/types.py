f8e37d42975dd53711386884b443ba6d92854406
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-02-13 03:05:02 -0300

    Quitando adapter espureo

--- src/pysmve/protocols/constructs/types.py
+++
@@ -1,7 +0,0 @@
-from construct import *
-
-if __name__ == '__main__':
-    ints = [0x2, 0x1, 0xff]
-    buffer = ''.join([chr(x) for x in ints])
-    X = BitStruct('foo', BitField('alfa', 24))
-    print X.parse(buffer)
