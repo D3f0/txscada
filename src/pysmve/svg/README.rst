f3b45a7cf32884e6159af0209d53213f219281b6
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-03-07 00:35:35 -0300

    Ejemplos de navegación svg en python

---
+++ src/pysmve/svg/README.rst
@@ -0,0 +1,4 @@
+Pruebas con SVG
+===============
+
+Pruebas de SVG con XPath para búsqueda de elementos
---
+++ src/pysmve/svg/svgparse.py
@@ -0,0 +1,21 @@
+from lxml.etree import ElementTree
+from functools import partial
+
+
+nsmap = {
+    'sodipodi': 'http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd',
+    'cc': 'http://web.resource.org/cc/',
+    'svg': 'http://www.w3.org/2000/svg',
+    'dc': 'http://purl.org/dc/elements/1.1/',
+    'xlink': 'http://www.w3.org/1999/xlink',
+    'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+    'inkscape': 'http://www.inkscape.org/namespaces/inkscape'
+}
+
+
+tree = ElementTree(file='source.svg')
+find = partial(tree.xpath, namespaces=nsmap)
+# print find('//svg:rect')
+
+
+print find('//svg:rect[@class="electric"]')
