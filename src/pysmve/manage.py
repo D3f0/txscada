ef0da081cfde490bd72225033abafbc58ca4e65f
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-04-12 02:21:32 -0300

    Cambios en el path

--- src/pysmve/manage.py
+++ src/pysmve/manage.py
@@ -3,8 +3,13 @@ import os
 import sys
 
 if __name__ == "__main__":
-    sys.path.append('./nguru')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nguru.settings")
+    # Monkey patch settings
+    sys.path.append(os.path.join(os.getcwd(), 'nguru'))
+    sys.path.append(os.path.join(os.getcwd(), 'nguru', 'nguru'))
+
+
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nguru.settings")
 
     from django.core.management import execute_from_command_line
 
