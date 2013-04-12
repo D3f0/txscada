6eed3e5d545c09a49b03603a69cc037f8fadaadb
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-04-12 02:22:16 -0300

    Paths y QBE en settings

--- src/pysmve/nguru/nguru/settings/base.py
+++ src/pysmve/nguru/nguru/settings/base.py
@@ -1,5 +1,4 @@
 # Django settings for nguru project.
-
 import sys
 from os.path import abspath, dirname, join
 
@@ -54,28 +53,32 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = join(PROJECT_ROOT, 'media')
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = join(PROJECT_ROOT, 'static-content')
+
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
+
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = '/media/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    #join(PROJECT_ROOT, 'static-conent'),
+    'static',
     join(PROJECT_ROOT, 'static'),
 )
 
@@ -132,6 +135,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'south',
+    # Graphical Query Browser
+
     'django_extensions',
     'django_socketio',
     'debug_toolbar',
@@ -144,9 +149,11 @@ INSTALLED_APPS = (
     'apps.research',
 
     #'admin_bootstrap',
-    'grappelli',
-    'django.contrib.admin',
+    'django_qbe',
+    'django_qbe.savedqueries',
 
+    #'grappelli',
+    'django.contrib.admin',
 )
 
 # A sample logging configuration. The only tangible logging
---
+++ src/pysmve/nguru/nguru/settings/qbe.py
@@ -0,0 +1,5 @@
+QBE_AUTOCOMPLETE = True
+QBE_EXHIBIT = False
+QBE_ADMIN = "admin"
+QBE_ADMIN_SITE ="admin.admin_site"
+QBE_ACCESS_FOR = lambda user: user.is_staff
