a5222b9b934e78d304ed9d97f7a7b0c51f52f27b
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-04-12 02:19:29 -0300

    Dump de requiremetns

--- src/pysmve/fabfile.py
+++ src/pysmve/fabfile.py
@@ -1,18 +1,19 @@
 # encoding: utf-8
-from fabric.api import local
+from fabric.api import local, task
 from os.path import join, dirname
 
 FAB_PATH = dirname(__file__)
 DEV_REQUIEREMENTS_FILE = join(FAB_PATH, 'requirements/develop.txt')
 
+@task
 def freeze():
     '''Freezar los requerimientos del virtualenv'''
-    with open(DEV_REQUIEREMENTS_FILE, 'wb') as f:    
+    with open(DEV_REQUIEREMENTS_FILE, 'wb') as f:
         reqs = local('pip freeze', True)
         f.write(reqs)
     print "Requirements written to %s" % DEV_REQUIEREMENTS_FILE
-        
 
+@task
 def docs():
 	'''Muestra la documentación'''
 	local('restview README.rst')
