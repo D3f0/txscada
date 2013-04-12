d906e31c1f86c28f8ac8233f8084a3bf5aaed5c3
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-04-12 12:18:26 -0300

    Change local_settings not to be commited

--- src/pysmve/nguru/nguru/settings/local_settings.example.py
+++ src/pysmve/nguru/nguru/settings/local_settings.example.py
@@ -7,7 +7,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'nguru', # Or path to database file if using sqlite3.
-        'USER': 'defo', # Not used with sqlite3.
+        'USER': 'nguru', # Not used with sqlite3.
         'PASSWORD': '', # Not used with sqlite3.
         'HOST': 'localhost', # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '', # Set to empty string for default. Not used with sqlite3.
