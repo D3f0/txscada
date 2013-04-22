5ab7f6255e990b040a42e48e73713c2037f98629
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-04-22 00:46:52 -0300

    Disable timezone

--- src/pysmve/nguru/nguru/settings/base.py
+++ src/pysmve/nguru/nguru/settings/base.py
@@ -49,7 +49,7 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
@@ -243,3 +243,8 @@ NOSE_ARGS = [
     #'--cprofile-stats-file=stats.stats',
     #'--processes=8'
 ]
+
+#import warnings
+#warnings.filterwarnings(
+#        'error', r"DateTimeField received a naive datetime",
+#        warnings.RuntimeWarning, r'django\.db\.models\.fields')
