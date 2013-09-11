536bfd2d94abd8a0ba2c78c96fb2479c5548fa93
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-09-10 23:49:17 -0300

    Event description

--- src/pysmve/nguru/apps/api/resources.py
+++ src/pysmve/nguru/apps/api/resources.py
@@ -1,52 +1,69 @@
 from tastypie.api import Api
-from tastypie.resources import ModelResource
+from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from tastypie import fields
 
 from apps.mara.models import (Profile, COMaster, IED, SV, DI, AI, Energy,
                               Event, )
 from apps.hmi.models import SVGScreen, Formula, SVGElement
 
 
+# API Entry Point
+api = Api(api_name='v1')
+
+
 class ProfileResource(ModelResource):
+
     """REST resource for Profile"""
     class Meta:
         resource_name = 'profile'
         queryset = Profile.objects.all()
         allowed_methods = ['get', ]
 
+api.register(ProfileResource())
+
 
 class COMasterResource(ModelResource):
+
     """REST resource for COMaster"""
     class Meta:
         resource_name = 'comaster'
         queryset = COMaster.objects.all()
         allowed_methods = ['get', ]
+api.register(COMasterResource())
 
 
 class IEDResource(ModelResource):
+
     """REST resource for IED"""
     class Meta:
         resource_name = 'ied'
         queryset = IED.objects.all()
         allowed_methods = ['get', ]
+api.register(IEDResource())
 
 
 class SVResource(ModelResource):
+
     """REST resource for SV"""
     class Meta:
         resource_name = 'sv'
         queryset = SV.objects.all()
         allowed_methods = ['get', ]
+api.register(SVResource())
 
 
 class DIResource(ModelResource):
+
     """REST resource for DI"""
     class Meta:
         resource_name = 'di'
         queryset = DI.objects.all()
         allowed_methods = ['get', ]
+api.register(DIResource())
 
 
 class EventResource(ModelResource):
+
     """REST resource for Event"""
     class Meta:
         resource_name = 'event'
@@ -55,16 +72,19 @@ class EventResource(ModelResource):
 
     def dehydrate(self, bundle):
         bundle.data['tag'] = bundle.obj.di.tag
-        bundle.data['texto'] = "Un buen texto"
+        bundle.data['texto'] = unicode(bundle.obj)
         return bundle
+api.register(EventResource())
 
 
 class AIResource(ModelResource):
+
     """REST resource for AI"""
     class Meta:
         resource_name = 'ai'
         queryset = AI.objects.all()
         allowed_methods = ['get', ]
+api.register(AIResource())
 
 
 # class EnergyPointResource(ModelResource):
@@ -76,45 +96,52 @@ class AIResource(ModelResource):
 
 
 class EnergyResource(ModelResource):
+
     """REST resource for Energy"""
     class Meta:
         resource_name = 'energy'
         queryset = Energy.objects.all()
         allowed_methods = ['get', ]
+api.register(EnergyResource())
 
 
 class SVGScreenResource(ModelResource):
+
     """REST resource for SVGScreen"""
     class Meta:
         resource_name = 'svgscreen'
         queryset = SVGScreen.objects.all()
         allowed_methods = ['get', ]
+        filtering = {
+            'name': ALL,
+            'id': ALL,
+        }
+api.register(SVGScreenResource())
+
 
 class FormulaResource(ModelResource):
+
     class Meta:
         resource_name = 'formula'
         queryset = Formula.objects.all()
         allowed_methods = ['get', ]
+api.register(FormulaResource())
+
 
 class SVGElementResource(ModelResource):
+    screen = fields.ForeignKey(SVGScreenResource, 'screen')
+
     class Meta:
         resource_name = 'svgelement'
         queryset = SVGElement.objects.all()
         allowed_methods = ['get', ]
         limit = 200
-
-
-api = Api(api_name='v1')
-
-api.register(ProfileResource())
-api.register(COMasterResource())
-api.register(IEDResource())
-api.register(SVResource())
-api.register(DIResource())
-api.register(EventResource())
-api.register(AIResource())
-api.register(FormulaResource())
-#api.register(EnergyPointResource())
-api.register(EnergyResource())
+        filtering = {
+            'screen': ALL_WITH_RELATIONS
+        }
 api.register(SVGElementResource())
-api.register(SVGScreenResource())
+
+
+
+
+# api.register(EnergyPointResource())
