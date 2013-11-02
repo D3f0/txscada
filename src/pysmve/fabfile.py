5bcd1edfadf41e37f982884c6469155d480a6dc8
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-11-02 18:19:34 -0300

    Setup

--- src/pysmve/fabfile.py
+++ src/pysmve/fabfile.py
@@ -1,37 +1,39 @@
 # encoding: utf-8
-from fabric.api import env, task, settings, run, local, cd
+from __future__ import print_function
+from os.path import dirname, join
+
+from bunch import Bunch, bunchify
+from fabric.api import abort, cd, env, local, run, settings, sudo, task, prefix
 from fabric.context_managers import quiet
+from fabric.contrib import files
 from fabric.contrib.console import confirm
-from os.path import join, dirname
-from bunch import bunchify, Bunch
+from fabric import colors
+from fabsettings import HOSTS as HOST_SETTINGS
 
 FAB_PATH = dirname(__file__)
 DEV_REQUIEREMENTS_FILE = join(FAB_PATH, 'requirements/develop.txt')
-_HOSTS = {
-    'virtualbox_coop': {
-        'remote_ip': '192.168.56.101',
-        'port': 22,
-        'user': 'ubuntu',
-        'password': 'ubuntu',
-        'virtualenv': 'txscada',
-        'project_base': 'src',
-        'project_name': 'txscada'
-    },
-    # 'demo': {
-    # }
-}
 
 def build_hosts(config_dict):
     hosts = Bunch()
     for name, values in config_dict.items():
+        if not values:
+            continue
         h = bunchify(values)
-        h.remote_location = join('/home', h.user, h.project_base, h.project_name)
+        h.project_path = join('/home', h.user, h.project_base, h.project_name)
         h.host_string = '{user}@{remote_ip}:{port}'.format(**h)
         h.venv_prefix = '/home/{user}/.virtualenv/{virtualenv}/bin/activate'.format(**h)
         hosts[name] = h
     return hosts
 
-HOSTS = build_hosts(_HOSTS)
+HOSTS = build_hosts(HOST_SETTINGS)
+
+def _check_host(host, hosts=HOSTS):
+    if isinstance(host, str):
+        if host not in hosts:
+            abort('Incorrect or empty host. Possible hosts: ' + ','.join(hosts.keys()))
+        return hosts[host]
+    else:
+        return hosts
 
 
 @task
@@ -40,13 +42,13 @@ def freeze():
     with open(DEV_REQUIEREMENTS_FILE, 'wb') as f:
         reqs = local('pip freeze', True)
         f.write(reqs)
-    print "Requirements written to %s" % DEV_REQUIEREMENTS_FILE
+    print("Requirements written to %s" % DEV_REQUIEREMENTS_FILE)
 
 
 @task
 def docs():
-	'''Muestra la documentación'''
-	local('restview README.rst')
+    '''Muestra la documentación'''
+    local('restview README.rst')
 
 
 def clean_repo():
@@ -59,17 +61,54 @@ def push_all():
     local('git push --all')
     local('git push --tags')
 
+# @task
+# def deploy_vm(host='virtualbox_coop', check_git='yes'):
+#     h = HOSTS.get(host, {})
+#     if check_git == 'yes':
+#         if not clean_repo() and not confirm("Deploy leaving unstaged changes?"):
+#             abort("Aborting at user request.")
+#     with settings(**h):
+#         push_all()
+#         with cd(env.remote_location):
+#             run('git pull origin master')
+#             run('git pull origin master')
+#             run('pwd')
+#             run('git submodule update --init')
+
+
+def create_virtualenv():
+    if not files.exists('/home/{user}/.virtualenvs/{virtualenv}'.format(**env)):
+        print("No existe el virtualenv %s" % colors.red(env['virtualenv']))
+        run('source /usr/local/bin/virtualenvwrapper.sh && mkvirtualenv {virtualenv}'.format(**env))
+
+
+def create_project_dir():
+    """Project path will contain the repository"""
+    if not files.exists(env.project_path):
+        run('mkdir -p {project_path}'.format(**env))
+
+def get_repo():
+    with cd(env.project_path):
+        if not files.exists('.git'):
+            run('git clone {repo_url} .'.format(**env))
+        else:
+            run('git checkout -- .')
+            run('git pull origin master')
+
+def install_dependencies():
+    with cd(env.project_path):
+        with prefix('workong {virtualenv}'.format(**env)):
+            run('pip install -r requirements.txt')
+
+
 @task
-def deploy_vm(host='virtualbox_coop', check_git='yes'):
-    h = HOSTS.get(host, {})
-    if check_git == 'yes':
-        if not clean_repo() and not confirm("Deploy leaving unstaged changes?"):
-            abort("Aborting at user request.")
+def install(host=''):
+    h = _check_host(host)
     with settings(**h):
-        push_all()
-        with cd(env.remote_location):
-            run('git pull origin master')
-            run('git pull origin master')
-            run('pwd')
-            run('git submodule update --init')
+        #run('ifconfig')
+        create_virtualenv()
+        create_project_dir()
+        get_repo()
+        install_dependencies()
+
 
---
+++ src/pysmve/fabutils/supervisor.py
@@ -0,0 +1,5 @@
+# encoding: utf-8
+
+
+def install_supervisor():
+    sudo('pip install supervisor')
