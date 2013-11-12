447ad9c51a753dfc36eb3ba1977acebf825b7056
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-11-12 19:30:04 -0300

    Use form to select SVGScreen

--- src/pysmve/nguru/apps/hmi/forms.py
+++ src/pysmve/nguru/apps/hmi/forms.py
@@ -1,11 +1,13 @@
 from django import forms
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
+from apps.hmi.models import SVGScreen
+from django.utils.translation import ugettext_lazy as _
 
 
 class EnergyDatePlotForm(forms.Form):
     date = forms.DateField(label="Fecha de la curva")
-    #estacion = forms.
+    # estacion = forms.
 
     def __init__(self, *args, **kwargs):
         super(EnergyDatePlotForm, self).__init__(*args, **kwargs)
@@ -17,3 +19,7 @@ class EnergyDatePlotForm(forms.Form):
         self.helper.form_action = 'submit_survey'
 
         #self.helper.add_input(Submit('submit', 'Submit'))
+
+
+class SVGScreenForm(forms.Form):
+    svg_screen = forms.ModelChoiceField(SVGScreen.objects.all(), label=_("SVG Screen"))
--- src/pysmve/nguru/apps/hmi/views.py
+++ src/pysmve/nguru/apps/hmi/views.py
@@ -1,26 +1,22 @@
 # encoding: utf-8
 
-import json
-
 from apps.hmi.forms import EnergyDatePlotForm
-from apps.hmi.models import SVGElement, SVGPropertyChangeSet, SVGScreen
-from django.shortcuts import get_object_or_404, redirect, render_to_response
+from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 
 # Imports para la simulación
 
 
-
 def realtime_watch(request):
     '''Visión de tiempo real'''
-    svg = SVGScreen.objects.latest('pk')
+    from forms import SVGScreenForm
+    form = SVGScreenForm()
 
     return render_to_response('hmi/realtime_watch.html',
                               context_instance=RequestContext(request, {
-                                                              'svg': svg,
+                                                              'form': form,
                                                               })
                               )
-
 
 
 def energy_plot(request, ):
