d66501a217270cf13d669325a66bfc0ed96f5972
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-11-14 13:28:17 -0300

    Allow PKs to be higher than one digit

--- src/pysmve/nguru/apps/hmi/urls.py
+++ src/pysmve/nguru/apps/hmi/urls.py
@@ -3,6 +3,6 @@ import views
 
 urlpatterns = patterns('',
     url('^realtime_watch/$', views.realtime_watch, name='realtime_watch'),
-    url('^svg_file/(?P<svg_pk>[\d+])/$', views.svg_file, name='svg_file'),
+    url('^svg_file/(?P<svg_pk>\d+)/$', views.svg_file, name='svg_file'),
     url('^energy_plot/$', 'apps.hmi.views.energy_plot', name='energy_plot'),
 )
