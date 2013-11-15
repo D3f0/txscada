cb56976ac4daa87f7ac10636cf10b182a7b42f0d
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-11-15 09:45:55 -0300

    Differential update

--- src/pysmve/nguru/apps/hmi/static/hmi/js/realtime_watch.js
+++ src/pysmve/nguru/apps/hmi/static/hmi/js/realtime_watch.js
@@ -1,18 +1,6 @@
 (function ($){
     $(function (){
 
-        var svg = null;
-
-        var formulas = {};
-
-        // var xhr = $.ajax('/api/v1/formula?format=json&limit=500');
-        // function retrieveFormulas (data) {
-        //     $.each(data.objects, function () {
-        //         console.log(this);
-        //     });
-        // }
-        // xhr.then(retrieveFormulas);
-
         function showDialogForNode(node) {
             var tag = $(node).attr('tag');
             var buttons = [
@@ -77,33 +65,39 @@
                 }
             });
         }
-        function serialize(obj) {
-          var str = [];
-          for(var p in obj)
-             str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-          return str.join("&");
+
+
+        var last_update = null;
+        var queryArguments = {};
+
+
+        function setQueryParams(obj){
+            queryArguments = obj || {};
         }
-        var last_update=null;
+
+        function updateQueryParams(obj) {
+            $.extend(queryArguments, obj || {});
+        }
+
+        function getQueryParamsEncoded() {
+            var str = [];
+            for(var p in queryArguments) {
+                var value = queryArguments[p];
+                if (value === null) {
+                    continue;
+                }
+                str.push(encodeURIComponent(p) + "=" + encodeURIComponent(value));
+
+            }
+            return str.join("&");
+        }
+
 
         function getSVGUpdatesUrl(extra_args){
-            extra_args = extra_args || {};
-            var baseArgs = {
-                format: 'json',
-                order_by: '-last_update',
-
-            }
-
-            if (typeof extra_args != "object") {
-                extra_args = {};
-            }
-            if (last_update) {
-                extra_args.last_update = last_update;
-            }
-
-            var baseUrl = '/api/v1/svgelement/';
-            var args = $.extend({}, baseArgs, extra_args);
-            var url = baseUrl + '?' + serialize(args);
-            console.log(url);
+            var baseUrl = Urls.api_dispatch_list('v1', 'svgelement');
+            updateQueryParams({'last_update': last_update});
+            var url = baseUrl + '?' + getQueryParamsEncoded();
+            console.info("Looking for updates", url);
             return url;
         }
 
@@ -140,24 +134,42 @@
             return dlg;
         }
 
+        var _svg = null;
+
+        function setCurrentSVG(svg) {
+            var $svg_attributes = $('#base', svg.root());
+            var $container_div = $(svg.root().parentElement);
+            var page_color = $svg_attributes.attr('pagecolor');
+            //var height = $svg_attributes.attr('inkscape:window-height');
+            var height = $(svg.root()).attr('height');
+            $container_div.css({'background': page_color, 'height': height+'px'});
+
+            _svg = svg;
+        }
+
+        function getCurrentSVG() {
+            return _svg;
+        }
+
         function update(){
             if (typeof(SMVE.updateInterval) == 'undefined') {
-                SMVE.updateInterval = 1000;
+                SMVE.updateInterval = 3000;
             }
             window.setTimeout(update, SMVE.updateInterval);
             if (!SMVE.update) {
                 return;
             }
             // Apply changes taken from the REST resource
+
             $.ajax({
                     url: getSVGUpdatesUrl(),
                     success: function (data, status){
                         var objs = data.objects;
                         var attributes = ['fill', 'color', 'text'];
-
+                        //console.info()
                         $.each(objs, function (idx, obj) {
                             //console.log(obj)
-                            var node = $('[tag='+obj.tag+']', svg.root());
+                            var node = $('[tag='+obj.tag+']', getCurrentSVG().root());
                             if (node.length)  {
                                 var updates = {};
                                 updates['text'] = obj.text;
@@ -165,7 +177,16 @@
                                 applyChanges(node, updates);
                             }
                         });
-
+                        // Update last update
+                        objs.sort(function (a, b){
+                            a = new Date(XDate.parse(a.last_update));
+                            b = new Date(XDate.parse(b.last_update));
+                            return a - b;
+                        });
+                        var last_update = objs[0].last_update;
+                        last_update = new XDate(XDate.parse(last_update));
+                        last_update = last_update.toString('yyyy-MM-dd HH:mm:ss.ffffff');
+                        updateQueryParams({last_update__gt: last_update});
                     },
                     error: showRESTErrorDialog
                 });
@@ -287,24 +308,6 @@
             return alarmGrid;
         }
 
-        function createUpdateIntervalSlider() {
-
-            $( "#SMVE-update-interval" ).slider({
-                value: 1000,
-                min: 250,
-                max: 10000,
-                step: 250,
-                value: SMVE.updateInterval,
-                slide: function( event, ui ) {
-                    //$( "#amount" ).val( "$" + ui.value );
-                    $('#update_interval').text(ui.value);
-                    SMVE.updateInterval = ui.value;
-                }
-            });
-            console.log($( "#SMVE-update-interval" ));
-            return false;
-        }
-
         function createTabs() {
             $("body").animate({ 'padding-top': 0}, 'slow');
             $('#tabs').tabs({
@@ -317,10 +320,6 @@
                         if (typeof(SMVE.alarmGrid) == 'undefined') {
                             SMVE.alarmGrid = createAlarmGrid();
                         }
-                        break;
-
-                    case 'tab-configuracion':
-                        createUpdateIntervalSlider();
                         break;
 
                     case 'tab-volver':
@@ -350,7 +349,15 @@
                 SMVE.updateButton.button().click(updateToggle);
             }
         }
-
+        function setUpdatesEnabled(enabled) {
+            console.info("Set updates", (enabled)?("enabled"):("disabled"));
+            if (enabled) {
+                SMVE.enabled = true;
+                update(); // Call the first time
+            } else {
+                SMVE.enabled = false;
+            }
+        }
 
         function svgScreenLoaded(svg) {
             var parent = svg._svg.parentElement;
@@ -358,11 +365,20 @@
             $('[tag]', svg.root()).click(function (){
                 showDialogForNode(this);
             });
+            setCurrentSVG(svg);
+            setUpdatesEnabled(true);
         }
 
         function changeScreen(){
             var svg_pk = $(this).val();
             var url = Urls.svg_file(svg_pk);
+            setUpdatesEnabled(false);
+
+            setQueryParams({
+                format: 'json',
+                svgscreen__pk: svg_pk
+            });
+
             console.info("Loading", url);
             $('#svg').removeClass('hasSVG').find('svg').remove();
             $('#svg').svg({
@@ -372,6 +388,7 @@
         }
 
         function bindSelectForScreens(){
+
             $('#id_svg_screen').bind('change', changeScreen);
         }
 
