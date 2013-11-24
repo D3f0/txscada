5a9436ffd6f3f1ab8d8329eb0104af6d9bd91267
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-11-24 19:37:54 -0300

    Auth for POST (alarms), new resource for eg detail )

--- src/pysmve/nguru/apps/api/resources.py
+++ src/pysmve/nguru/apps/api/resources.py
@@ -1,15 +1,14 @@
-from tastypie.api import Api
-from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
+from apps.hmi.models import Formula, SVGElement, SVGScreen
+from apps.mara.models import AI, COMaster, DI, Energy, Event, IED, Profile, SV
 from tastypie import fields
-
-from apps.mara.models import (Profile, COMaster, IED, SV, DI, AI, Energy,
-                              Event)
-from apps.hmi.models import SVGScreen, Formula, SVGElement
-
+from tastypie.api import Api
+from tastypie.authorization import Authorization
+from tastypie.resources import ALL, ALL_WITH_RELATIONS, ModelResource
+from datetime import datetime
 
 # API Entry Point
 api = Api(api_name='v1')
-
+auth = Authorization()
 
 class ProfileResource(ModelResource):
 
@@ -81,11 +80,40 @@ class EventResource(ModelResource):
             'timestamp_ack': ALL,
         }
         ordering = Event._meta.get_all_field_names()
+        authorization = auth
+        order_by = ('-timestamp')
 
     def dehydrate(self, bundle):
         bundle.data['tag'] = bundle.obj.di.tag
         bundle.data['texto'] = unicode(bundle.obj)
         return bundle
+
+    def obj_update(self, bundle, skip_errors=False, **kwargs):
+        """
+        A ORM-specific implementation of ``obj_update``.
+        """
+        if not bundle.obj or not self.get_bundle_detail_data(bundle):
+            try:
+                lookup_kwargs = self.lookup_kwargs_with_identifiers(bundle, kwargs)
+            except:
+                # if there is trouble hydrating the data, fall back to just
+                # using kwargs by itself (usually it only contains a "pk" key
+                # and this will work fine.
+                lookup_kwargs = kwargs
+
+            try:
+                bundle.obj = self.obj_get(bundle=bundle, **lookup_kwargs)
+            except Event.ObjectDoesNotExist:
+                raise NotFound("A model instance matching the provided arguments could not be found.")
+
+        self.authorized_update_detail(self.get_object_list(bundle.request), bundle)
+
+        if bundle.data.get('timestamp_ack', None) == 'now':
+            bundle.data['timestamp_ack'] = datetime.now()
+
+        bundle = self.full_hydrate(bundle)
+        return self.save(bundle, skip_errors=skip_errors)
+
 
 
 api.register(EventResource())
@@ -126,6 +154,7 @@ class SVGScreenResource(ModelResource):
     """REST resource for SVGScreen"""
 
     profile = fields.ForeignKey(ProfileResource, 'profile')
+    parent = fields.ForeignKey('self', 'parent', null=True, blank=True)
 
     class Meta:
         resource_name = 'svgscreen'
@@ -174,16 +203,42 @@ class SVGElementResource(ModelResource):
         order_by = 'last_update'
 
 
-    def apply_filters(self, request, applicable_filters):
-        #import pdb; pdb.set_trace()
-        return super(SVGElementResource, self).apply_filters(request, applicable_filters)
-
     def dehydrate(self, bundle):
         bundle.data['style'] = bundle.obj.style
         return bundle
 
 api.register(SVGElementResource())
 
+
+class SVGElementDetailResource(ModelResource):
+    screen = fields.ForeignKey(SVGScreenResource, 'screen')
+    on_click_jump = fields.ForeignKey(SVGScreenResource,
+                                      'on_click_jump',
+                                      blank=True,
+                                      null=True,
+                                      )
+
+    formulas = fields.ToManyField(FormulaResource, 'formula_set', full=True)
+
+    def dehydrate(self, bundle):
+        #bundle.data['formulas'] = bundle.obj.formulas
+        return bundle
+
+    class Meta:
+        resource_name = 'svgelementdetail'
+        queryset = SVGElement.objects.select_related('formula', 'screen')
+        allowed_methods = ['get', ]
+        limit = 200
+        filtering = {
+            'screen': ALL_WITH_RELATIONS,
+            'last_update': ALL_WITH_RELATIONS,
+            'enabled': ALL_WITH_RELATIONS,
+            'on_click_jump': ALL,
+        }
+        ordering = SVGElement._meta.get_all_field_names()
+        order_by = 'last_update'
+
+api.register(SVGElementDetailResource())
 
 
 
