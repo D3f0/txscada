86a02e4b83ec177800669bd60c2edbe8315b66ad
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-11-26 14:13:16 -0300

    Remove color choices

--- src/pysmve/nguru/apps/hmi/forms.py
+++ src/pysmve/nguru/apps/hmi/forms.py
@@ -63,22 +63,28 @@ def make_svg_change_label(p):
 class SVGElementForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super(SVGElementForm, self).__init__(*args, **kwargs)
-        self.populate_color_choices()
+    #     self.populate_color_choices()
 
-    def populate_color_choices(self):
-        qs = SVGPropertyChangeSet.objects.all()
+    # def populate_color_choices(self):
+    #     qs = SVGPropertyChangeSet.objects.all()
 
-        choices = [(None, '--------')]
+    #     choices = [('', '--------')]
 
-        choices += [
-            (p.index, make_svg_change_label(p)) for p in qs
-        ]
-        self.fields['fill'].choices=choices
-        self.fields['stroke'].choices=choices
+    #     choices += [
+    #         (p.index, make_svg_change_label(p)) for p in qs
+    #     ]
+    #     self.fields['fill'].choices=choices
+    #     self.fields['stroke'].choices=choices
 
 
-    fill = forms.ChoiceField(required=False, label=_('fill').title())
-    stroke = forms.ChoiceField(required=False, label=_('stroke').title())
+    # fill = forms.IntegerField(
+    #                          required=False,
+    #                          label=_('fill').title()
+    #                         )
+    # stroke = forms.IntegerField(
+    #                             required=False,
+    #                             label=_('stroke').title(),
+    #                           )
 
     def clean_linked_text_change(self):
         screen = self.cleaned_data.get('screen')
@@ -100,6 +106,10 @@ class SVGElementForm(forms.ModelForm):
         }
 
 
+class AlarmFilterForm(forms.Form):
+    date_from = forms.DateField()
+    date_ = forms.DateField()
+
 class FormuluaInlineForm(forms.ModelForm):
 
     class Meta:
@@ -109,3 +119,91 @@ class FormuluaInlineForm(forms.ModelForm):
                                                       'style': 'width: 100%',})
         }
         fields = ('attribute', 'formula', )
+
+
+
+MIN_PASS_LEN = 6
+MAX_PASS_LEN = 24
+EXTRA_PASS_SYMBOLS = u'!"#$%&/()=?¡-_'
+VALID_PASS_CHARS = ''.join([string.letters, string.digits, EXTRA_PASS_SYMBOLS])
+
+
+class UserForm(forms.ModelForm):
+
+    def __init__(self, *largs, **kwargs):
+        super(UserForm, self).__init__(*largs, **kwargs)
+        instance = kwargs.get('instance', None)
+        if instance:
+            self.fields['pass_1'].required = False
+            self.fields['pass_2'].required = False
+
+    pass_1 = forms.CharField(label='Clave', widget=forms.PasswordInput,
+                             help_text="Calve de acceso al sistema, la longitud mínima son %d caracteres y la máxima %d" % (MIN_PASS_LEN, MAX_PASS_LEN))
+    pass_2 = forms.CharField(label='Repetir calve', widget=forms.PasswordInput)
+
+    user_permissions = forms.ModelMultipleChoiceField(Permission.objects.all(),
+                                                      #widget=jQueryUIMultiSelect(filter=True)
+                                                      )
+
+    def poner_error_en_los_pass(self, mensaje):
+        ''' Acceso a bajo nivel de los errores de los campos, sirve para
+        poener de manera automática en varios campos un mismo error'''
+        error = [mensaje, ]
+        self._errors['pass_1'] = self.error_class(error)
+        self._errors['pass_2'] = self.error_class(error)
+
+    def clean(self):
+        # Contraseña
+        pass_1 = self.cleaned_data.get('pass_1')
+        pass_2 = self.cleaned_data.get('pass_2')
+
+        # import ipdb; ipdb.set_trace()
+        if pass_1 or pass_2:
+            # Primero deben ser iguales
+            if pass_1 != pass_2:
+                self.poner_error_en_los_pass('Las claves deben ser iguales')
+            else:
+                self.es_clave_usable(pass_1)
+
+        return self.cleaned_data
+
+
+    def es_clave_usable(self, clave):
+        # Mayor al mínimo
+        if len(clave) < MIN_PASS_LEN:
+            self.poner_error_en_los_pass('La calve no debe ser inferior a %d caracteres' % MIN_PASS_LEN)
+            return False
+        # Menos al maximo
+        elif len(clave) > MAX_PASS_LEN:
+            self.poner_error_en_los_pass('La calve no debe ser superior a %d caracteres' % MAX_PASS_LEN)
+            return False
+        # Caracteres validos
+        elif not(all(map(lambda c: c in VALID_PASS_CHARS, clave))):
+            self.poner_error_en_los_pass(u'La clave solo puede contener letras, números y los símbolos %s' % EXTRA_PASS_SYMBOLS)
+            return False
+        # No puede user el nombre de usuario
+        else:
+            username = self.cleaned_data.get('username')
+            if clave.count(username) > 0:
+                self.poner_error_en_los_pass(u'No se puede user el nombre de usuario como clave ni como parte de ella')
+                return False
+        return True
+
+    def save(self, *largs, **kwargs):
+        user = super(UserForm, self).save(*largs, **kwargs)
+        try:
+            self.instance.pk
+            user.set_password(self.cleaned_data['pass_1'])
+            user.is_staff = True
+        except:
+            #_pk = None
+            pass
+        user.save()
+        return user
+
+    class Meta:
+        model = User
+        exclude = ('password', 'user_permissions', )
+        widgets = {
+            'groups': forms.CheckboxSelectMultiple,
+        }
