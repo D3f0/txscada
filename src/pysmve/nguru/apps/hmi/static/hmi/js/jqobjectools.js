e3a9ad267e553f432646e9cf9065f04c2c0cad64
Nahuel Defosse <nahuel.defosse@gmail.com>
2013-11-27 11:56:18 -0300

    Missing files

---
+++ src/pysmve/nguru/apps/hmi/static/hmi/js/jqobjectools.js
@@ -0,0 +1,20 @@
+$.fn.serializeObject = function()
+{
+   var o = {};
+   var a = this.serializeArray();
+   $.each(a, function() {
+       if (o[this.name]) {
+           if (!o[this.name].push) {
+               o[this.name] = [o[this.name]];
+           }
+           if (this.value) {
+            o[this.name].push(this.value || '');
+           }
+       } else {
+        if (this.value) {
+           o[this.name] = this.value || '';
+         }
+       }
+   });
+   return o;
+};
