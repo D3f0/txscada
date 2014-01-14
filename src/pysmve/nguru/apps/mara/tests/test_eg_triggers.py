0f7ba71429a5102dffec7396a1d49e2cf25f9189
Nahuel Defosse <nahuel.defosse@gmail.com>
2014-01-14 17:13:01 -0300

    Misc forgotten testing

--- src/pysmve/nguru/apps/mara/tests/test_eg_triggers.py
+++ src/pysmve/nguru/apps/mara/tests/test_eg_triggers.py
@@ -13,8 +13,10 @@ class DigitalEventUpdatesSVGElementTextTest(TestCase):
         self.comaster = COMasterFactory(profile=self.profile)
         self.ied = IEDFactory(co_master=self.comaster)
         description = 'Test DI'
-        self.di = self.ied.di_set.create(port=0, bit=0, description=description)
+        self.di = self.ied.di_set.create(port=0, bit=0, description=description,
+                                         tag="DITAG0")
 
 
     def test_di_events_are_propagated(self):
+        import nose; raise nose.SkipTest()
         assert False
