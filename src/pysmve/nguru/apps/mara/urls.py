47adbbb0f10410cc22b497018c2718269f9c02ef
Nahuel Defosse <nahuel.defosse@gmail.com>
2014-02-25 11:51:16 -0300

    Add model tree to URLs

--- src/pysmve/nguru/apps/mara/urls.py
+++ src/pysmve/nguru/apps/mara/urls.py
@@ -3,12 +3,14 @@
 from django.conf.urls import patterns, url
 #from .updates import register
 
-urlpatterns = patterns('',
-
-    # Análisis de tramas mara
-    url(r'mara_frame_analizer',
-        'apps.mara.views.mara_frame_analizer',
+urlpatterns = patterns(
+    'apps.mara.views',
+    url(r'^mara_frame_analizer/$',
+        'mara_frame_analizer',
         name='mara_frame_analizer'),
+    url('^mara_model_tree/$',
+        'mara_model_tree',
+        name='mara_model_tree'),
 )
 
 
