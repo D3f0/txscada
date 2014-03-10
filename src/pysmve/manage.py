b469187447b8e477eeed6ced741fedbe72fb8af6
Nahuel Defosse <nahuel.defosse@gmail.com>
2014-03-10 10:23:17 -0300

    Move path change to settings instead of manage.py (not used in WSGI)

--- src/pysmve/manage.py
+++ src/pysmve/manage.py
@@ -4,12 +4,6 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nguru.nguru.settings")
-    # Monkey patch settings
-    sys.path.append(os.path.join(os.getcwd(), 'nguru'))
-    sys.path.append(os.path.join(os.getcwd(), 'nguru', 'nguru'))
-
-
-    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nguru.settings")
 
     from django.core.management import execute_from_command_line
 
--- src/pysmve/nguru/nguru/settings/__init__.py
+++ src/pysmve/nguru/nguru/settings/__init__.py
@@ -1,9 +1,17 @@
 # Monkey patch local path
 import sys
+from os.path import abspath, join, dirname
 
+# This is made so Django project is able to be used from upper immediate directory
+# structure
+_path = abspath(join(dirname(__file__), '../../..'))
+sys.path.append(join(_path, 'nguru'))
+sys.path.append(join(_path, 'nguru/nguru'))
+
+# Basic settings
 from base import *
-if DEBUG:
-    sys.path.append(join(PROJECT_ROOT, '..', '..', '..'))
+#if DEBUG:
+#    sys.path.append(join(PROJECT_ROOT, '..', '..', '..'))
 from zmq import *
 from websockets import *
 # Django query by example
