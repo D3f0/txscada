7bc2808271d2e869a51641b421980b2d37c08c3f
Nahuel Defosse <nahuel.defosse@gmail.com>
2014-03-12 16:39:36 -0300

    i18n in form field

--- src/pysmve/nguru/apps/mara/forms.py
+++ src/pysmve/nguru/apps/mara/forms.py
@@ -2,12 +2,14 @@
 from django import forms
 from models import COMaster
 from django.core.exceptions import ValidationError
+from django.utils.translation import ugettext as _
 
 
 class ManualFrameInsertForm(forms.Form):
     co_master = forms.ModelChoiceField(COMaster.objects.all())
-    frame = forms.CharField(widget=forms.Textarea)
-
+    frame = forms.CharField(widget=forms.Textarea(attrs={'cols': 100,
+                            'class': 'field span9',
+                            'placeholder': _('Paste frame here')}))
 
     def clean_frame(self):
         data = self.cleaned_data.get('frame')
