ab095cf3a7da7f06dae543880d3b7aac439898f4
Nahuel Defosse <nahuel.defosse@gmail.com>
2014-03-26 00:16:40 -0300

    Formula changelist JS for calculate

--- src/pysmve/nguru/apps/hmi/static/hmi/js/formula_admin_changelist.js
+++ src/pysmve/nguru/apps/hmi/static/hmi/js/formula_admin_changelist.js
@@ -1,21 +1,34 @@
 (function($){
-    $(function(){
-        //alert($.fn.jquery);
-        $('.svg_popup').click(function (event){
-            event.preventDefault();
-            var $this = $(this),
-                title = $this.attr('tag'),
-                options = {
-                    modal: true,
-                    title: title,
-                    autoOpen: false,
-                    close: function(){
-                        $(this).dialog('destroy');
-                    }
-                },
-                $dlg = $('<div/>').dialog(options);
-                $dlg.dialog('open');
+    function onCalculateOnClick(event) {
+        event.preventDefault();
+        var $this = $(this);
+        try {
+            var data = JSON.parse($(this).attr('data'));
+        } catch (e) {
+            console.error("Cannot read data from link", e);
+            return;
+        };
 
-        });
+        var $dlg = $('<div/>').dialog({
+            autoOpen: false,
+            modal: true,
+            title: data.title,
+            buttons: [
+                {
+                    text: "Close",
+                    click: function () {
+                        $dlg.dialog('close');
+                    }
+                }
+            ],
+            close: function () {
+                console.log("Destroy");
+                $(this).dialog('destroy');
+            }
+        }).html('<p class="loading"/>');
+        $dlg.dialog('open');
+    }
+    $(function(){
+        $('.do_calculate').click(onCalculateOnClick);
     });
 })(jQuery || django.jQuery);
