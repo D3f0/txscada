368c5670768859c379e519ee5d9dcd8a5aba0c75
Nahuel <nahuel.defosse@gmail.com>
2014-05-14 02:40:06 -0300

    Add disabled i18n

--- src/pysmve/nguru/nguru/urls.py
+++ src/pysmve/nguru/nguru/urls.py
@@ -3,19 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
-
+from django.views.i18n import javascript_catalog
 from apps.mara.admin import site
 from apps.api.resources import api
-
-
 import object_tools
+
 
 object_tools.autodiscover()
 
+js_info_dict = {
+    'packages': ('apps.mara', 'apps.hmi', 'apps.api', ),
+}
 
 app_urls = patterns('',
 
@@ -78,6 +76,9 @@ app_urls = patterns('',
     # Django object tools
     #=========================================================================================
     (r'^object-tools/', include(object_tools.tools.urls)),
+
+    #(r'^jsi18n/(?P<packages>\S+?)/$', 'django.views.i18n.javascript_catalog'),
+
 )
 
 
@@ -100,10 +101,3 @@ if settings.DEBUG:
                           )
 
     urlpatterns += staticfiles_urlpatterns()
-
-# if settings.DEBUG:
-#     urlpatterns += patterns('',
-#         (r'^(?P<template>.*)/?$', 'django.views.generic.simple.direct_to_template', ),
-#     )
-
-
