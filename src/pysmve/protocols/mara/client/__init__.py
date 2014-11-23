e0a833d32d947cbed9c64cc8354647e80c1616b9
Nahuel <nahuel.defosse@gmail.com>
2014-11-22 23:23:44 -0300

    Remove old code

--- src/pysmve/protocols/mara/client/__init__.py
+++ src/pysmve/protocols/mara/client/__init__.py
@@ -1,6 +1,2 @@
 from base import MaraClientProtocol, MaraClientProtocolFactory
 from dbupdater import MaraClientDBUpdater
-try:
-    from redis import MaraClientPackageRedisPublisher
-except ImportError:
-    pass
