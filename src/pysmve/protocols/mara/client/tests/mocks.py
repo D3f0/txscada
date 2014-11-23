d18033f8c59ec4e775cc82d570d2c186b36d2d40
Nahuel <nahuel.defosse@gmail.com>
2014-11-23 00:31:19 -0300

    Add missing fields to mock

--- src/pysmve/protocols/mara/client/tests/mocks.py
+++ src/pysmve/protocols/mara/client/tests/mocks.py
@@ -8,6 +8,8 @@ def COMasterMock():
     for a protocol/clientfactory to work.
     '''
     comaster = MagicMock()
+    comaster.ip_address = '127.0.0.1'
+    comaster.port = 9761
     comaster.sequence = None
     comaster.poll_interval = 5
     comaster.rs485_destination = 1
