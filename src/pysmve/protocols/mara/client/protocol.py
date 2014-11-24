004e794ffc171d13b21febdd89fa2df6f57cc13d
Nahuel <nahuel.defosse@gmail.com>
2014-11-24 10:54:57 -0300

    Nicer docs

--- src/pysmve/protocols/mara/client/protocol.py
+++ src/pysmve/protocols/mara/client/protocol.py
@@ -1,9 +1,9 @@
-'''
+"""
 Portocol implementation that dialogues with a Mara COMaster
 It takes advantage of Twisted's inlineCallbacks
 http://hackedbellini.org/development/writing-asynchronous-python-code-with-twisted-using-inlinecallbacks/
-Features some basic reassembler if data comes paritioned.
-'''
+Features some basic reassembler if data comes chunked.
+"""
 from __future__ import print_function
 from twisted.internet import protocol
 from twisted.internet import defer, reactor
@@ -25,19 +25,25 @@ LOGGER_NAME = 'commands'
 
 class MaraPorotocolFactory(protocol.ReconnectingClientFactory):
 
-    def __init__(self, comaster, **options):
-        '''
+    def __init__(self, comaster):
+        """
         This class should be instantiated by a Django management command but since
         we don't want it to have hard dependencies on Django code, we leave some
         attributes to be handled by
-        '''
+        """
         self.comaster = comaster
+        # The handlers will be set by the management command that instanciates this
+        # factory, and will be executed when a POLL frame is received.
         self.handlers = []
         logger = logging.getLogger(LOGGER_NAME)
+        # This adapter prints the IP of the comaster with every log message
         self.logger = COMasterLogAdapter(logger, {'comaster': self.comaster})
         self.attemps = 0
 
     def get_configured_construct(self):
+        """
+        Takes from configuration the path to the ConsturctClass
+        """
         class_ = import_class(get_setting('MARA_CONSTRUCT'))
         return class_
 
@@ -51,7 +57,11 @@ class MaraPorotocolFactory(protocol.ReconnectingClientFactory):
         return class_
 
     def buildProtocol(self, addr):
-        '''Constructs protocol based on configuration'''
+        """
+        This method is called when a connection is made. A transient protocol
+        that will handle *only* one session will be generated.
+        :param addr: The address of the connection
+        """
         self.resetDelay()
         self.attemps = 0
         protocol_class = self.get_configured_protocol()
@@ -62,6 +72,11 @@ class MaraPorotocolFactory(protocol.ReconnectingClientFactory):
         return instance
 
     def connectTCP(self, reactor, override_ip=None):
+        """
+        Connects the ClientFactory instance to a reactor.
+        :param reactor: The reactor to bind to
+        :param override_ip: The ip to be overriden.
+        """
         return reactor.connectTCP(
             host=override_ip or self.comaster.ip_address,
             port=self.comaster.port,
@@ -70,10 +85,16 @@ class MaraPorotocolFactory(protocol.ReconnectingClientFactory):
         )
 
     def startedConnecting(self, connector):
+        """
+        Twisted event handler. Logging purpouses.
+        """
         protocol.ReconnectingClientFactory.startedConnecting(self, connector)
         self.logger.info("Connecting to %s...", connector.getDestination())
 
     def clientConnectionFailed(self, connector, reason):
+        """
+        Twisted event handler. Logging purpouses.
+        """
         self.attemps += 1
         protocol.ReconnectingClientFactory.clientConnectionFailed(self, connector, reason)
         msg = unicode(reason).replace('\n', ' ')
@@ -82,11 +103,16 @@ class MaraPorotocolFactory(protocol.ReconnectingClientFactory):
 
 
 class ConnectionLost(Exception):
-    '''Used for singaling in callbacks'''
+    """
+    Used for singaling in callbacks
+    """
     pass
 
 
 class Timeout(Exception):
+    """
+    Used for errback in defereds that exepct input from the other side
+    """
     pass
 
 
@@ -117,7 +143,9 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
         self._state = new_state
 
     def sendCotainer(self, container):
-        '''Convenience method for publishing when data is sent'''
+        """
+        Convenience method for publishing when data is sent
+        """
         # TODO: Publish COMASTER, STATE, DATA
         assert isinstance(container, Container)
         data = self.construct.build(container)
@@ -126,10 +154,13 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
 
     @property
     def comaster(self):
-        '''Shortcut to comaster instance'''
+        """
+        Shortcut to comaster instance
+        """
         return self.factory.comaster
 
     def setUp(self):
+        """Initialization"""
         self.state = self.States.STARTED
         # Sequence
         s = self.comaster.sequence
@@ -140,10 +171,15 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
 
     @property
     def active(self):
+        """Flag that checks if the main loop can be executed"""
         return self.state not in (self.States.CONNECTION_LOST, self.States.GAVE_UP)
 
     @defer.inlineCallbacks
     def mainLoop(self):
+        """
+        Main loop that executes the comunication. It tries to interleave every
+        resposability the reactor has.
+        """
         while self.active:
             yield self.doPEH()
             replied = yield self.doPoll()
@@ -156,15 +192,26 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
         self.transport.loseConnection()
 
     def waitForNextPollOrUserCommands(self):
+        """
+        Created a defered that will be callbacked form somewhere else indicating
+        what shuold be done.
+        """
         self.waitingDefered = defer.Deferred()
         reactor.callLater(self.comaster.poll_interval, self.waitingDefered.callback, None)
         return self.waitingDefered
 
     def connectionMade(self):
+        """
+        Called by twsited when the connection is made. The main loop is not implemented
+        here for clarity reasons and testabilty using the reactor. Calls setup.
+        """
         self.setUp()
         reactor.callLater(0, self.mainLoop)
 
     def buildPollContainer(self):
+        """
+        Creates a mara container using information of the comaster reference.
+        """
         return Container(
             source=self.comaster.rs485_source,
             dest=self.comaster.rs485_destination,
@@ -174,6 +221,9 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
         )
 
     def buildPeHContainer(self, timestamp):
+        """
+        Creates a PEH container.
+        """
         container = Container(
             source=self.comaster.rs485_source,
             dest=0xFF,
@@ -184,6 +234,10 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
         return container
 
     def pepreareToReceive(self):
+        """
+        Check if the connection is able to recieve data, if not ConnectionLost is risen.
+        Created
+        """
         if self.state == self.States.CONNECTION_LOST:
             raise ConnectionLost()
         self.input_buffer.reset()
@@ -196,6 +250,12 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
 
     @defer.inlineCallbacks
     def doPoll(self):
+        """
+        Sends Poll commands and waits for reply.
+        It supports data to be chunked. It times out.
+
+        :return bool: True if data could be retrieved from device, False otherwise.
+        """
         self.state = self.States.SEND_POLL
 
         tries, max_tries = 0, self.comaster.max_retry_before_offline
@@ -246,10 +306,17 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
 
     @defer.inlineCallbacks
     def doUserCommands(self):
+        """
+        To be implemeted
+        """
         yield defer.returnValue(None)
 
     @defer.inlineCallbacks
     def doPEH(self):
+        """
+        Sends PEH if more than COMaster.peh_
+
+        """
         self.state = self.States.CHECK_NEED_PEH
         if self.comaster.needs_peh():
             self.state = self.States.SEND_PEH
@@ -259,6 +326,10 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
         yield defer.returnValue(None)
 
     def dataReceived(self, data):
+        """
+        Called when data is available. It'll callback the Deferred that are waiting
+        for input if it can reasseble a mara frame.
+        """
         self.logger.info("%s << %s", self.state, upperhexstr(data))
         # Add data that could be chunked to the buffer
         self.input_buffer += data
@@ -278,10 +349,17 @@ class MaraClientProtocol(object, protocol.Protocol, TimeoutMixin):
                 self.logger.info("Unexpected package %s", upperhexstr(package))
 
     def timeoutConnection(self):
+        """
+        Called when connection is lost.
+        Errbacks incoming data Deferreds.
+        """
         if self.incomingDefered:
             self.incomingDefered.errback(Timeout())  # data = yield -> None (in callee)
 
     def connectionLost(self, reason):
+        """
+        Called when connection is lost. errbacks the incoming Deferreds.
+        """
         self.logger.warning("Connection lost in %s reason: %s", self.state, reason)
         self.state = self.States.CONNECTION_LOST
         self.setTimeout(None)
