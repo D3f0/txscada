855182d4bbe75444c868e2d3ea11968a3bb16489
Nahuel <nahuel.defosse@gmail.com>
2014-11-24 23:55:17 -0300

    Pair settings

--- src/pysmve/nguru/nguru/settings/base.py
+++ src/pysmve/nguru/nguru/settings/base.py
@@ -177,7 +177,7 @@ LOGGING = {
             'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'  # noqa
         },
         'simple': {
-            'format': '%(levelname)s %(message)s'
+            'format': '%(asctime)s %(levelname)s %(message)s'
         },
     },
 
