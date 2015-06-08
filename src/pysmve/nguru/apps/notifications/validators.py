83c2324825ad07d94e20e4d1cdecfb9b4f11e26c
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-06-08 03:54:20 -0300

    Validate better

--- src/pysmve/nguru/apps/notifications/validators.py
+++ src/pysmve/nguru/apps/notifications/validators.py
@@ -1,12 +1,12 @@
 from django.core.exceptions import ValidationError
 from django.template import Template, Context
-
+from constance import config
 
 def validate_template_format(template_string):
     """Validates template django format"""
     # TODO: Fix possible ORM DML access
     try:
         template = Template(template_string)
-        template.render(Context())
+        template.render(Context({'config': config}))
     except Exception as e:
         raise ValidationError(unicode(e))
