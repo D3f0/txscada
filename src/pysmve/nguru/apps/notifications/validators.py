e998e7a3ecd606ab46cf05403052f23a4e90b81d
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-06-08 23:31:06 -0300

    PEP8

--- src/pysmve/nguru/apps/notifications/validators.py
+++ src/pysmve/nguru/apps/notifications/validators.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ValidationError
 from django.template import Template, Context
 from constance import config
 
+
 def validate_template_format(template_string):
     """Validates template django format"""
     # TODO: Fix possible ORM DML access
