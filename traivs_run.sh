78c1548251c15370166d12836d4c8eabdb5f6141
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-06-10 23:51:28 -0300

    Some tunning on .travis.yml

--- traivs_run.sh
+++ traivs_run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# This script is porinted by script option in .travis.yml. This should be run on travis
+# on every commit to master branch.
+# Based on https://gist.github.com/ndarville/3625246
 # Break on first error
 set -e
 
