263bb17b51afed76708a5be4a42d39eb2ee6f5d6
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-07-18 01:42:54 -0300

    Rewrite some old tests

--- src/pysmve/protocols/mara/client/tests/test_protocol_poll.py
+++ src/pysmve/protocols/mara/client/tests/test_protocol_poll.py
@@ -1,18 +1,32 @@
-from twisted.trial import unittest
+# from twisted.trial import unittest
+from nguru.apps.mara.tests.factories import SMVETreeCOMaseterFactory
+from django.test import TestCase
+from twisted.test import proto_helpers
 
 
-class TestPoll(unittest.TestCase):
+class TestPoll(TestCase):
 
     def setUp(self):
-        pass
+        self.co_master = SMVETreeCOMaseterFactory()
+        self.proto_factory = self.co_master.get_protocol_factory()
+
+    def _make_protocol(self):
+        proto = self.proto_factory.buildProtocol(('127.0.0.1', 0))
+        tr = proto_helpers.StringTransport()
+        proto.makeConnection(tr)
+        return proto, tr
 
     def test_poll_timeout(self):
-        assert False
+        pass
 
     def test_poll_can_receive_chunked_data(self):
         assert False
 
     def test_poll_updates_squence(self):
+        # Based on
+        # http://twistedmatrix.com/documents/12.3.0/core/howto/trial.html
+        proto, tr = self._make_protocol()
+
         assert False
 
     def test_poll_calls_handler_when_package_is_received(self):
