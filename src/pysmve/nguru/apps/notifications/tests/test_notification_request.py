2e64b7ef233823513e6da8f4087301d72bcbe6bc
Nahuel Defosse <nahuel.defosse@gmail.com>
2015-07-20 22:01:16 -0300

    Test for utility function for modem retrieval

--- src/pysmve/nguru/apps/notifications/tests/test_notification_request.py
+++ src/pysmve/nguru/apps/notifications/tests/test_notification_request.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from datetime import datetime
 from django.core.urlresolvers import reverse
 from django_webtest import WebTest
-from apps.notifications.utils import SMSServerToolsModem, TIME_FORMAT
+from apps.notifications.utils import SMSServerToolsModem, get_available_modem
 import tempfile
 import shutil
 from unittest import skip
@@ -116,26 +116,72 @@ class TestNotificationFilesCreatedForSMSServerToolsModem(TestCase):
     def setUp(self):
         self.path = tempfile.mkdtemp('smve-outoging-sms')
         self.modem = SMSServerToolsModem(outoging_queue=self.path)
+        self.modem.get_current_time_formated = mock.MagicMock()
+        self.fixed_date = '20120101235959'
+        self.modem.get_current_time_formated.return_value = self.fixed_date
 
-    def test_sms_created_for_sms_server_tools_without_id(self, patched):
-
-        fixed_date = '20120101235959'
-        patched.return_value = fixed_date
+    def test_sms_created_for_sms_server_tools_without_id(self):
 
         to = 5492804123456
         message = 'Esta es una prueba'
         self.modem.send_sms(to, message)
         expedted_file_path = os.path.join(
-            self.path, fixed_date + '-' + to + 'NA'
+            self.path, self.fixed_date + '-' + str(to) + '-' + 'NA'
         )
         assert os.path.exists(expedted_file_path)
+        with open(expedted_file_path) as fp:
+            data = fp.read()
+        import pdb; pdb.set_trace()
+        os.unlink(expedted_file_path)
 
+    def test_sms_created_for_sms_server_tools_with_id(self):
+
+        to = 5492804123456
+        msg_id = 1234
+        message = 'Esta es una prueba'
+        self.modem.send_sms(to, message, msg_id)
+        expedted_file_path = os.path.join(
+            self.path, self.fixed_date + '-' + str(to) + '-' + str(msg_id)
+        )
+        assert os.path.exists(expedted_file_path)
+        os.unlink(expedted_file_path)
 
     def tearDown(self):
         shutil.rmtree(self.path, ignore_errors=True)
 
+    def test_create_message_file(self):
+        NotificationRequest.objects.all().delete()
+        with self.settings():
+            pass
 
-#@skip(u"Falta evaluar que falla con WebTest")
+
+class ModemA(object):
+    """Dummy class"""
+    pass
+
+
+class ModemB(object):
+    """Dummy class"""
+    pass
+
+
+class TestModemConfiguration(TestCase):
+    def setUp(self):
+        pass
+
+    def test_configuration(self):
+
+        with self.settings(SMS_MODEM_CLASS='nguru.apps.notifications.tests.'
+                           'test_notification_request.ModemA'):
+            available_modem = get_available_modem()
+            self.assertIsInstance(available_modem, ModemA)
+        with self.settings(SMS_MODEM_CLASS='nguru.apps.notifications.tests.'
+                           'test_notification_request.ModemB'):
+            available_modem = get_available_modem()
+            self.assertIsInstance(available_modem, ModemB)
+
+
+@skip(u"Falta evaluar que falla con WebTest")
 class TestUserProfileShowsInternationalNumbers(WebTest):
     def setUp(self):
         self.user = UserFactory(username="admin", is_superuser=True, is_staff=True)
@@ -143,10 +189,11 @@ class TestUserProfileShowsInternationalNumbers(WebTest):
         self.url = reverse('admin:auth_user_change', args=(self.user1.pk, ))
 
     def test_phonenumber_is_validated(self):
+        from apps.hmi.models import UserProfile
         response = self.app.get(self.url, user=self.user)
         response.form.fields['userprofile_set-0-cellphone'] = '123'
-        response = response.form.submit().follow()
+        response.showbrowser()
+
+        response = response.form.submit('_save').follow()
         profile = self.user1.get_profile()
-        import ipdb; ipdb.set_trace()
-
-
+        assert False
