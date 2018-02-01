7dc307a334498f7fc204a699f987ccb3299e68b3
Nahuel Defossé <nahuel.defosse@gmail.com>
2018-02-01 16:55:37 -0300

    Add collectstatic and syncdb to entrypoint

--- django_container/entrypoint.sh
+++ django_container/entrypoint.sh
@@ -3,17 +3,27 @@ set -e
 cmd="$@"
 
 if [ "${ENV:prod}" == "dev" ]; then
-    if [ "${NO_INSTALL}" != "1" ]; then
-        echo "Installing development requirements:"
-        pip install --user -r /setup/packages/dev.txt \
+    if [ -z "${NO_INSTALL}" ]; then
+        echo "Installing development requirements in $(hostname):"
+        pip install --user -r /pip_download/dev/dev.txt \
             --no-index \
-            --find-links /setup/packages/
+            --find-links /pip_download/dev/
         echo "Finished installing dev packages"
     else
         echo "Skipping installing development packages because of NO_INSTALL=1"
     fi
-    export PYTHONPATH=${PYTHONPATH}:${HOME}/.local/lib/python2.7/site-packages/
-    export PATH=${PATH}:${HOME}/.local/bin
+    
+    # Done in docker-compose
+    # export PYTHONPATH=${PYTHONPATH}:${HOME}/.local/lib/python2.7/site-packages/
+    # export PATH=${PATH}:${HOME}/.local/bin
+fi
+
+if [ -z "${NO_MIGRATE}" ]; then
+    python manage.py syncdb --migrate --noinput
+fi
+
+if [ -z "${NO_STATIC}" ]; then
+    python manage.py collectstatic --noinput
 fi
 
 exec $cmd
